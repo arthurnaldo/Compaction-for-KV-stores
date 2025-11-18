57a9485bf5ebe7aaf2e728ecd1dc4e635572bdab
Atharv Naidu <atharvan.naidu@outlook.com>
2025-11-18 14:45:50 -0800

    improved API skeleton

--- operations/main.go
+++ operations/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
-	"operations/compact"
+	"operations/get"
+	"operations/set"
 
 	"github.com/gin-gonic/gin"
 )
 
 // subprocess that listens to set requests, and sends them to set module
 func set_request(c *gin.Context) {
+	key, key_err := c.Get("key")
+	value, val_err := c.Get("value")
+	if key_err || val_err {
+		panic("no key or value provided")
+	}
+
+	k := key.(string)
+	v := value.(string)
+
+	set.Set(k, v)
 
 }
 
 // subprocess that listens to get requests, and sends them to get module
 func get_request(c *gin.Context) {
+	key, key_err := c.Get("key")
+	if key_err {
+		panic("no key or value provided")
+	}
+
+	k := key.(string)
+
+	println(get.Get(k))
 
 }
 
 func main() {
-	compact.Compact()
 
 	router := gin.Default()
 	router.GET("/value", get_request)
